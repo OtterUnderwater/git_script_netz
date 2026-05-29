15e30be2430eadeebcd089cd968fcf2802982a58
OtterUnderwater <teo2005@bk.ru>
2026-05-29 17:33:46 +0300

    fix: исправление небольших багов, проблемы с привязкой и округления

---
+++ create_new/Новый статус.sql
@@ -0,0 +1,8 @@
+INSERT INTO mes.cns_OrderState (OrderStateID, OrderStateName)
+VALUES (1, 'ќжидание')
+
+ALTER TABLE mes.WorkCenter
+ADD Sort INT NULL
+
+ALTER TABLE crm.Client_Item
+ADD EffectiveDate DATE NULL;
---
+++ тест_думаем.sql
@@ -0,0 +1,7 @@
+select * from wrh.docs --ТН
+select * from wrh.Request --ТК
+select * from wrh.RequestRow --ТК
+select * from wrh.docrows --ТН
+
+-- Добавить в базу + в запросы столбец сортировка (для сортировки рабочих мест) - использовать ее в плане
+select * from mes.WorkCenter
